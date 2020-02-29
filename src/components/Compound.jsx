e2de0ee423cfce0b115f1977d7cdd788f04dfb10
m dary nur <m.dary.nur@gmail.com>
2020-02-29 10:48:31 +0700

    add table

--- src/components/Compound.jsx
+++ src/components/Compound.jsx
@@ -2,6 +2,7 @@ import React, { memo, useState } from 'react'
 import Select from './Select'
 import FieldNumber from './FieldNumber'
 import Chart from './Chart'
+import Table from './Table'
 
 const currencies = [
 	{ value: '£', label: 'GBP' },
@@ -40,44 +41,46 @@ const Compound = () => {
 					<div className='container mx-auto px-6 py-32 relative'>
 						<h3 className='flex flex-col items-center text-4xl text-secondary font-bold text-center pb-12'>Let's calculate <span className='bg-primary h-1 w-20 block mt-4'></span></h3>
 
-						<div className='flex flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row justify-between'>
-
-							<div className='mb-24 md:mb-16 xl:mb-8 sm:w-full md:w-1/3'>
-								<form onSubmit={handleSubmit} name='contact' method='post' data-netlify='true' data-netlify-honeypot='bot-field' className='w-full'>													
-									<div className='mb-6'>
-										<div className='w-full mb-6 px-4'>
-											<label className='block mb-2 text-copy-primary font-bold' htmlFor='currency'>Currency</label>
-											<Select name='currency' id='currency' value={formData.currency} onChange={handleDropdown} items={currencies} itemId='value' itemLabel={o => `${o.label} (${o.value})`} />
-										</div>
-
-										<div className='w-full mb-6 px-4'>
-											<label className='block text-copy-primary mb-2 font-bold' htmlFor='period'>Investment Period (Years)</label>
-											<FieldNumber type='number' onFocus={e => e.target.select()} name='period' id='period' value={formData.period} onChange={handleChange} max={100} maxLength={3} />
-										</div>
-
-										<div className='w-full mb-6 px-4'>
-											<label className='block mb-2 text-copy-primary font-bold' htmlFor='growth'>Annual Growth Rate</label>
-											<Select name='growth' id='growth' value={formData.growth} onChange={handleDropdown} items={rates} itemId='value' itemLabel='label' />
-										</div>
-
-										<div className='w-full mb-6 px-4'>
-											<label className='block text-copy-primary mb-2 font-bold' htmlFor='contribution'>Monthly Contribution ({formData.currency})</label>
-											<FieldNumber type='number' onFocus={e => e.target.select()} name='contribution' id='contribution' value={formData.contribution} onChange={handleChange} max={100} maxLength={3} data-right-label={formData.currency} />
-										</div>
+						<div className='mb-24 md:mb-16 xl:mb-8 sm:w-full w-full'>
+							<form onSubmit={handleSubmit} name='contact' method='post' data-netlify='true' data-netlify-honeypot='bot-field' className='w-full'>													
+								<div className='flex flex-wrap mb-6 -mx-4'>
+									<div className='w-full md:w-1/2 mb-6 px-4'>
+										<label className='block mb-2 text-copy-primary font-bold' htmlFor='currency'>Currency</label>
+										<Select name='currency' id='currency' value={formData.currency} onChange={handleDropdown} items={currencies} itemId='value' itemLabel={o => `${o.label} (${o.value})`} />
 									</div>
-								</form>
-							</div>
 
-							<div className='mb-24 md:mb-16 xl:mb-8 sm:w-full md:w-2/3'>
+									<div className='w-full md:w-1/2 mb-6 px-4'>
+										<label className='block text-copy-primary mb-2 font-bold' htmlFor='period'>Investment Period (Years)</label>
+										<FieldNumber type='number' onFocus={e => e.target.select()} name='period' id='period' value={formData.period} onChange={handleChange} max={100} maxLength={3} />
+									</div>
+								</div>
+								<div className='flex flex-wrap mb-6 -mx-4'>
+									<div className='w-full md:w-1/2 mb-6 px-4'>
+										<label className='block mb-2 text-copy-primary font-bold' htmlFor='growth'>Annual Growth Rate</label>
+										<Select name='growth' id='growth' value={formData.growth} onChange={handleDropdown} items={rates} itemId='value' itemLabel='label' />
+									</div>
+
+									<div className='w-full md:w-1/2 mb-6 px-4'>
+										<label className='block text-copy-primary mb-2 font-bold' htmlFor='contribution'>Monthly Contribution ({formData.currency})</label>
+										<FieldNumber type='number' onFocus={e => e.target.select()} name='contribution' id='contribution' value={formData.contribution} onChange={handleChange} max={100} maxLength={3} data-right-label={formData.currency} />
+									</div>
+								</div>
+							</form>
+						</div>
+
+						<div className='flex flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row justify-between'>							
+							<div className='mb-24 mx-4 md:mb-16 xl:mb-8 sm:w-full md:w-2/3'>
 								<div className='bg-white shadow rounded w-full'>
 									<Chart {...formData} />
 								</div>
 							</div>
 
-						</div>
-						
-
-
+							<div className='mb-24 mx-4 md:mb-16 xl:mb-8 sm:w-full md:w-1/3'>
+								<div className='bg-white shadow rounded w-full'>
+									<Table {...formData} />
+								</div>
+							</div>
+						</div>					
 					</div>
 				</div>
 			</div>
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -4,7 +4,7 @@ import separator from '../helpers/separator'
 
 const Table = props => {
 
-    const [variable, setVariable] = useState({ period: 0, growth: 0, contribution: 0 })
+	const [variable, setVariable] = useState({ period: 0, growth: 0, contribution: 0 })
 	const { period, growth, contribution, currency } = variable
 
 	useEffect(() => {
@@ -13,7 +13,7 @@ const Table = props => {
 			clearTimeout(handler)
 		})
 	}, [props, variable, setVariable])
-	
+
 	const data = useMemo(
 		() => {
 			const result = []
@@ -31,31 +31,32 @@ const Table = props => {
 			return result
 		},
 		[period, growth, contribution]
-    )
-    
-    return (
-        <div className='bg-white shadow p-4 w-full'>
-            <h3 className='text-xl font-bold text-blue-900'>Breakdown</h3>
-	        {data.length > 0 && <h3 className='text-md font-bold text-secondary text-blue-900'>in {period} years, you will have {`${currency} ${separator(data[data.length - 1].value.toFixed(2))}`}</h3>}
-            <HTMLTable striped bordered className='w-full'>
-                <thead>
-                    <tr>
-                    <th>End of year</th>
-                    <th>Value</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map((o, i) => (
-                        <tr key={i}>
-                            <td>{o.label}</td>
-                            <td>{`${currency} ${separator(o.value.toFixed(2))}`}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </HTMLTable>
+	)
 
-        </div>
-    )
+	return (
+		<div className='px-2 py-4'>
+			<h3 className='text-xl font-bold text-center'>Breakdown</h3>
+			{data.length > 0 && <h3 className='text-md text-secondary text-center mt-2 text-gray-300'>in {period} years, you will have {`${currency} ${separator(data[data.length - 1].value.toFixed(2))}`}</h3>}
+			<div className='overflow-x-auto h-16' style={{ height: '21.8rem' }}>
+				<table className='text-left w-full border-collapse'>
+					<thead>
+						<tr>
+							<th className='py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light'>End Of Year</th>
+							<th className='py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light'>Value ({currency})</th>
+						</tr>
+					</thead>
+					<tbody>
+						{data.map((o, i) => (
+							<tr key={i} className='hover:bg-grey-lighter'>
+								<td className='py-4 px-6 border-b border-grey-light'>{o.label}</td>
+								<td className='py-4 px-6 border-b border-grey-light'>{`${currency} ${separator(o.value.toFixed(2))}`}</td>
+							</tr>
+						))}										
+					</tbody>
+				</table>
+			</div>
+		</div>			
+	)
 }
 
 export default memo(Table)
