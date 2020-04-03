46319f88a2c2270aa3f8abd567926a8a54190f8e
m-dary-nur <m.dary.nur@gmail.com>
2020-04-03 08:50:17 +0700

    disable sendgrid service

--- functions/sendmail.js
+++ functions/sendmail.js
@@ -1,6 +1,6 @@
-const sgMail = require('@sendgrid/mail')
+// const sgMail = require('@sendgrid/mail')
 const querystring = require('querystring')
-const { SG_API_KEY, SG_CC_EMAIL, SG_TO_EMAIL, SG_SENDER_EMAIL } = process.env
+// const { SG_API_KEY, SG_CC_EMAIL, SG_TO_EMAIL, SG_SENDER_EMAIL } = process.env
 
 exports.handler =  async (event, context, callback) => {
 
@@ -10,39 +10,44 @@ exports.handler =  async (event, context, callback) => {
 
     const payload = querystring.parse(event.body);
     
-    sgMail.setApiKey(SG_API_KEY)
+    // sgMail.setApiKey(SG_API_KEY)
 
-    // const body = Object.keys(payload).map((k) => {
-    //     return `${k}: ${payload[k]}`
-    // }).join("<br><br>");
+    // // const body = Object.keys(payload).map((k) => {
+    // //     return `${k}: ${payload[k]}`
+    // // }).join("<br><br>");
+
+    // // const msg = {
+    // //     to: SENDGRID_TO_EMAIL,
+    // //     from: email,
+    // //     subject: subject ? subject : 'Contact Form Submission',
+    // //     html: body,
+    // // };
 
     // const msg = {
-    //     to: SENDGRID_TO_EMAIL,
-    //     from: email,
-    //     subject: subject ? subject : 'Contact Form Submission',
-    //     html: body,
-    // };
+    //     to: SG_TO_EMAIL,
+    //     cc: SG_CC_EMAIL,
+    //     from: SG_SENDER_EMAIL,
+    //     subject: 'Sending with SendGrid is Fun',
+    //     text: 'and easy to do anywhere, even with Node.js',
+    //     html: `<strong>and easy to do anywhere, even with Node.js</strong><br /> your submitted data is "${JSON.stringify(payload)}"`,
+    //   };
 
-    const msg = {
-        to: SG_TO_EMAIL,
-        cc: SG_CC_EMAIL,
-        from: SG_SENDER_EMAIL,
-        subject: 'Sending with SendGrid is Fun',
-        text: 'and easy to do anywhere, even with Node.js',
-        html: `<strong>and easy to do anywhere, even with Node.js</strong><br /> your submitted data is "${JSON.stringify(payload)}"`,
-      };
-
-    try{
-        await sgMail.send(msg)
+    // try{
+    //     await sgMail.send(msg)
         
-        return {
-            statusCode: 200,
-            body: "Message sent"
-        }
-    } catch(e){
-        return {
-            statusCode: e.code,
-            body: e.message
-        }
+    //     return {
+    //         statusCode: 200,
+    //         body: "Message sent"
+    //     }
+    // } catch(e){
+    //     return {
+    //         statusCode: e.code,
+    //         body: e.message
+    //     }
+    // }
+
+    return {
+        statusCode: 200,
+        body: "Message sent"
     }
 };
